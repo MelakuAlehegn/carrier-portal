ef53ae16058c9c4173620ee63afe9c7f9b838bf9
unknown <melakualehegn34@gmail.com>
2023-11-16 04:27:50 +0300

    initial setup

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const cors = require('cors')
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+connectDB()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use(errorHandler)
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
