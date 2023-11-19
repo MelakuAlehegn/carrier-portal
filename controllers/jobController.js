ded6f48e9310bccd1efb0fcccd7f1f400ac29d15
unknown <melakualehegn34@gmail.com>
2023-11-19 21:39:24 +0300

    create job, update job api

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const { Job } = require('../models/jobsModel')
+const { Job, validateJob } = require('../models/jobsModel')
 
 const getJobs = async (req, res) => {
     let { page, limit } = req.query
@@ -20,4 +20,28 @@ const getJob = async (req, res) => {
     }
     return res.status(200).json(job)
 }
-module.exports = { getJobs, getJob }
+
+const createJob = async (req, res) => {
+    const { error } = validateJob(req.body)
+    if (error) {
+        return res.status(400).json(error.details[0].message)
+    }
+    const job = await Job.create(req.body)
+    return res.status(201).json(job)
+}
+
+const updateJob = async (req, res) => {
+    const { id } = req.params
+    if (!mongoose.isValidObjectId(id)) {
+        return res.status(400).json({ error: 'Invalid job ID format' })
+    }
+    const job = await Job.findById(id)
+    if (!job) {
+        return res.status(404).json({ message: "Job not found" })
+    }
+    const updatedJob = await Job.findByIdAndUpdate(id, req.body, {
+        new: true
+    })
+    res.status(200).json(updatedJob)
+}
+module.exports = { getJobs, getJob, createJob, updateJob }
--- routes/api/jobRoutes.js
+++ routes/api/jobRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { getJobs, getJob } = require('../../controllers/jobController')
+const { getJobs, getJob, createJob, updateJob } = require('../../controllers/jobController')
 
 const router = express.Router()
 router.get('/', getJobs)
 router.get('/:id', getJob)
-
+router.post('/', createJob)
+router.put('/:id', updateJob)
 
 module.exports = router
 
